500d57eb8f56dcb0fdf0b9ed7db2de3e7d0d42ca
blackfight1 <1677445796@qq.com>
2026-02-25 22:24:34 +0800

    2

--- internal/db/models.go
+++ internal/db/models.go
@@ -1,11 +1,42 @@
 package db
 
 import (
+	"database/sql/driver"
+	"errors"
 	"time"
 
-	"gorm.io/datatypes"
 	"gorm.io/gorm"
 )
+
+// JSONB 自定义 JSONB 类型
+type JSONB []byte
+
+// Value 实现 driver.Valuer 接口
+func (j JSONB) Value() (driver.Value, error) {
+	if len(j) == 0 {
+		return nil, nil
+	}
+	return string(j), nil
+}
+
+// Scan 实现 sql.Scanner 接口
+func (j *JSONB) Scan(value interface{}) error {
+	if value == nil {
+		*j = nil
+		return nil
+	}
+	var bytes []byte
+	switch v := value.(type) {
+	case []byte:
+		bytes = v
+	case string:
+		bytes = []byte(v)
+	default:
+		return errors.New("failed to scan JSONB value")
+	}
+	*j = bytes
+	return nil
+}
 
 // Asset 资产模型
 type Asset struct {
@@ -15,7 +46,7 @@ type Asset struct {
 	IP           string         `json:"ip"`
 	StatusCode   int            `json:"status_code"`
 	Title        string         `json:"title"`
-	Technologies datatypes.JSON `json:"technologies"` // 存储技术栈数组
+	Technologies JSONB          `gorm:"type:jsonb" json:"technologies"` // 存储技术栈数组
 	LastSeen     time.Time      `json:"last_seen"`
 	CreatedAt    time.Time      `json:"created_at"`
 	UpdatedAt    time.Time      `json:"updated_at"`
